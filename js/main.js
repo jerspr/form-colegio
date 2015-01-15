c0c88b205ad38286a007dc54643b15225411f96f
jesser pariona <jerspr@gmail.com>
2015-01-14 23:01:50 -0500

    üpdate

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	$("#datosbasicos").validate({
+        // Specify the validation rules
+        rules: {
+            nombre: "required",
+            apellidop: "required",
+            apellidom: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            nombreal: "required",
+            apellidopal: "required",
+            apellidomal: "required",
+            nivel: "required",
+            grado: "required"
+        },
+        
+        // Specify the validation error messages
+        messages: {
+            nombre: "Ingrese su nombre",
+            apellidop: "Ingrese su apellido paterno",
+            apellidom: "Ingrese su apellido materno",
+            email:{
+            	required:"Ingrese su correo electronico",
+            	email:"Ingrese un correo electronico valido"
+            },
+            nombreal: "Ingrese su nombre",
+            apellidopal: "Ingrese su apellido paterno",
+            apellidomal: "Ingrese su apellido materno",
+            nivel: "Elija el nivel",
+            grado: "Elija el grado"
+        },
+        
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+});
